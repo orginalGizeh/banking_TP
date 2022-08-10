22e2ddf8bd6f17f7915d296be6868f825364c383
gizeh <jordanmafwefwe@gmail.com>
2022-08-10 15:30:31 +0200

    feat : add attributes of paramters Database

---
+++ src/java/com/kalebmaf/utils/ParametresConnexionSQL.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kalebmaf.utils;
+
+/**
+ *
+ * @author Gizeh
+ */
+public class ParametresConnexionSQL {
+    
+    //  attribut
+    private String urlBD;
+    private String user;
+    private String psssword;
+    
+    
+}
