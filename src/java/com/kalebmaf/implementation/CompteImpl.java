923e8f5dd0fa3397f8d07e58916565bc04c8db5a
gizeh <jordanmafwefwe@gmail.com>
2022-08-10 16:28:57 +0200

    feat : add override methods in CompteImpl

--- src/java/com/kalebmaf/implementation/CompteImpl.java
+++ src/java/com/kalebmaf/implementation/CompteImpl.java
@@ -5,10 +5,24 @@
  */
 package com.kalebmaf.implementation;
 
+import com.kalebmaf.bean.Compte;
+import com.kalebmaf.interfaces.ICompte;
+import java.util.ArrayList;
+
 /**
  *
  * @author Gizeh
  */
-public class CompteImpl {
+public class CompteImpl implements  ICompte{
+    
+    @Override
+    public Compte getCompte(String code) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Compte> getAllComptes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
