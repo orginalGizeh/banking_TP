654da2838edf2e3882d7d22eb956596d35bb3e97
gizeh <jordanmafwefwe@gmail.com>
2022-08-10 20:59:06 +0200

    feat : test unitaire

--- src/java/com/kalebmaf/utils/Test.java
+++ src/java/com/kalebmaf/utils/Test.java
@@ -24,15 +24,25 @@ public class Test {
         // TODO code application logic here
         BanqueImpl b = new BanqueImpl();
         CompteImpl c = new CompteImpl();
+        Banque ba = new Banque();
         ArrayList<Banque> lesBanques = b.getAllBanque();
-        System.out.println(lesBanques.toString());
-//        for (Banque lesBanque : lesBanques) {
-//            
-//        }
+        //ArrayList<Compte> lesComptes = c.getAllComptes();
+        //System.out.println(lesBanques.toString());
+        //System.out.println(lesComptes.toString());
+        //System.out.println(b.getBanque(new Banque("EQ10")));
+        for (Banque banque : lesBanques) {
+            System.out.println("*********************");
+            System.out.println(banque.getCodeBanque()+ " : " + banque.getNomBanque());
+            System.out.println("");
+            ArrayList<Compte> lesComptes = c.getAllComptesByCodeBanque(banque.getCodeBanque());
+            for (Compte compte : lesComptes) {
+                System.out.println("*" + compte.getCodeCompte() + " : " + compte.getTypeCompte() + " ( Solde : " + compte.getSolde()+ ")"  );
+            }
+        }
 //        ArrayList<Compte> lesComptes = c.getAllComptesByCodeBanque(lesBanques.get(0).getCodeBanque());
 //        for (Compte lesCompte : lesComptes) {
 //            
 //        }
     }
-    
+
 }
